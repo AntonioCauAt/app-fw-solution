0f193a7756670321e022d86bd57f70ae852d5e6b
AntonioCauAt <antoniocaulinatienzar@gmail.com>
2024-07-31 10:59:15 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,63 @@
+# set up environment
+import os
+import json
+import mne
+import helper
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+# Current path
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+# Load brainlife config.json
+with open('config.json','r') as config_f:
+    config = helper.convert_parameters_to_None(json.load(config_f))
+    
+
+fname = config['mne']
+raw = mne.io.read_raw_fif(fname, preload=True)
+subject = 'output'
+subj_dir = config['output']
+
+# # # SOURCE SPACE # # #
+#Assume that coregistration is done
+mne.set_log_level('WARNING')
+src = mne.setup_source_space(subject, spacing='oct6', subjects_dir=subj_dir,add_dist=False)
+
+
+#Visualization?
+#mne.viz.plot_alignment(raw, trans_fname, subject=subject, dig=False, src=src,subjects_dir=subj_dir, verbose=True, meg=False,eeg=False);
+                             
+
+# # # FORWARD SOLUTION # # #
+
+#Compute Bem Model
+conductivity = (0.3,)  # for single layer (MEG)
+# conductivity = (0.3, 0.006, 0.3)  # for three layers (EEG)
+model = mne.make_bem_model(subject=subject, ico=4  conductivity=conductivity, subjects_dir=subj_dir)
+bem = mne.make_bem_solution(model)
+
+
+#Compute Forward Model
+fwd = mne.make_forward_solution(fname, trans=trans_fname,
+src=src, bem=bem,
+meg=True,  # include MEG channels
+eeg=False,  # exclude EEG channels
+mindist=5.0,  # ignore sources <= 5mm from inner skull
+n_jobs=1)  # number of jobs to run in parallel
+
+#How each point in the brain space contributes to the signal measured at each sensor
+leadfield = fwd['sol']['data']
+
+#Save fwd
+fwd_fname = os.path.join(subject, fname)
+mne.write_forward_solution(fwd_fname, fwd, overwrite=True)
+
+#Save report
+report = mne.Report(title='Report')
+report_path = os.path.join('out_dir_report', 'report.html')
+report.save(report_path, overwrite=True)
